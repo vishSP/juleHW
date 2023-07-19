604d2695ebb16b189db3fb698dd0e574557b5268
vishnSP <hihi64741@gmail.com>
2023-07-19 15:58:07 +0400

    19 июля (проблема с тестами)

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAdminUser
 
 from users.models import User
 from users.permissions import IsUserProfile
-from users.serializers import UserSerializer, CustomTokenObtainPairSerializer, AuthUserSerializer
+from users.serializers import UserSerializer, AuthUserSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 class UserViewSet(viewsets.ModelViewSet):
