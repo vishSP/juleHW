60e73c2148c4bbac491664e96dde2f2352581e1d
vishnSP <hihi64741@gmail.com>
2023-07-31 14:07:56 +0400

    31 июля (готовый вариант)

--- vinsky/views.py
+++ vinsky/views.py
@@ -1,6 +1,6 @@
 from django_filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-from requests import Response
+from rest_framework.response import Response
 from rest_framework import viewsets, generics, status
 from rest_framework.permissions import IsAdminUser
 
@@ -90,11 +90,11 @@ class PaymentIntentCreateView(generics.CreateAPIView):
                 payment_intent = StripeService.create_payment_intent(course_id, user_id)
                 payment = Payments.objects.get(payment_intent_id=payment_intent['id'])
                 payment_serializer = PaymentSerializer(payment)
-                return Response(payment_serializer.data)
+                return Response(payment_serializer.data, status=status.HTTP_201_CREATED)
             except Exception as e:
-                return Response({'error': str(e)})
+                return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PaymentMethodCreateView(generics.CreateAPIView):
@@ -109,27 +109,24 @@ class PaymentMethodCreateView(generics.CreateAPIView):
                 StripeService.connection(payment_intent_id, payment_token)
                 payment = Payments.objects.get(payment_intent_id=payment_intent_id)
                 payment_serializer = PaymentSerializer(payment)
-                return Response(payment_serializer.data)
+                return Response(payment_serializer.data, status=status.HTTP_201_CREATED)
             except Exception as error:
-                return Response({'error': str(error)})
-        return Response(serializer.errors)
-
-
+                return Response({'error': str(error)}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PaymentIntentConfirmView(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
 
-        serializer = PaymentMethodCreateSerializer(data=request.data)
+        serializer = PaymentIntentConfirmSerializer(data=request.data)
         if serializer.is_valid():
             payment_intent_id = serializer.validated_data['payment_intent_id']
-            payment_token = serializer.validated_data['payment_token']
             try:
-                StripeService.connection(payment_intent_id, payment_token)
+                StripeService.confirm_payment_intent(payment_intent_id)
                 payment = Payments.objects.get(payment_intent_id=payment_intent_id)
                 payment_serializer = PaymentSerializer(payment)
-                return Response(payment_serializer.data)
+                return Response(payment_serializer.data, status=status.HTTP_201_CREATED)
             except Exception as error:
-                return Response({'error': str(error)})
-        return Response(serializer.errors)
+                return Response({"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
