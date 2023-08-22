0e095545469f527c771a49c6667622b535c6c669
vishnSP <hihi64741@gmail.com>
2023-08-22 17:27:49 +0400

    22 августа

--- config/settings.py
+++ config/settings.py
@@ -101,11 +101,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': os.getenv('POSTGRES_DB'),
-         'HOST': os.getenv('POSTGRES_HOST'),
-         'PORT': os.getenv('POSTGRES_PORT'),
-         'USER': os.getenv('POSTGRES_USER'),
-         'PASSWORD': os.getenv('POSTGRES_PASSWORD')
+         'NAME': 'jule_db',
+         'USER': 'postgres',
     }
 }
 
